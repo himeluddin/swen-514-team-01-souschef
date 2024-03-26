18f095911224cbf58cdb1826a9229ac147c84acf
Mackenzie Wade <mackenziewade@mackenziesair5.wireless.rit.edu>
2024-03-25 23:14:37 -0400

    sucky code but at least it is a little usefull :)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
 function App() {
     return (
         <div>
             <h1 class="text-3xl font-bold underline">
                 Hello world!
-            </h1>
+
+            </h1 >
+
         </div>
 
-    );
+
+
+);
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import RecipeView from "./RecipeView";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+      < RecipeView/>
   </React.StrictMode>
 );
 
