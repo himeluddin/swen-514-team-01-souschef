74ed5a73bee4cb28529125bf9fb2e192abdfb822
DJSR59 <smithrods59@gmail.com>
2024-03-28 18:40:24 -0400

    Added Edit Modal button

--- frontend/src/components/IngredientCard.js
+++ frontend/src/components/IngredientCard.js
@@ -1,8 +1,12 @@
 import apple from '../imgs/apple.jpg'
 import close from '../imgs/close.png'
 import '../css/CameraComponent.css'
+import { useRef } from 'react';
+import { EditModal } from './EditModal';
 
 function IngredientCard() {
+    const editModalRef = useRef()
+
     return (
         <div>
             <div class="p-3 px-10 max-w-sm w-full lg:max-w-full lg:flex">
@@ -17,11 +21,12 @@ function IngredientCard() {
                         </div>
                     <div class="text-gray-900 font-bold text-4xl">Apple</div>
                     <div class="flex items-center card-button">
-                        <button class="static card-button inline-flex p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+                        <button onClick={() => editModalRef.current.openModal()} class="static card-button inline-flex p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
                             <span class="static px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                                 Edit
                             </span>
                         </button>
+                        <EditModal ref={editModalRef}/>
                     </div>
                 </div>
             </div>
