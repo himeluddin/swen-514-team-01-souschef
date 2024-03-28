956e209f34a429d991127528bdb3dcad72310947
DJSR59 <smithrods59@gmail.com>
2024-03-28 14:41:47 -0400

    Added the Add Button. Also pushing up the Symbols/Test Pictures I'm using

--- frontend/src/components/IngredientCard.js
+++ frontend/src/components/IngredientCard.js
@@ -12,14 +12,14 @@ function IngredientCard() {
                 </div>
                 <div class="w-full border border-gray-400 lg:border-gray-400 bg-white rounded-tr rounded-br p-4 flex flex-col justify-between leading-normal">
                         <div class="flex items-center card-button">
-                            <button class="h-12 w-12 p-0 mt-2 mr-2 relative transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                            <button class="h-12 w-12 p-0 static transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                                 <img src={close} alt="Close Button"></img>
                             </button>
                         </div>
                     <div class="text-gray-900 font-bold text-4xl">Apple</div>
                     <div class="flex items-center card-button">
-                        <button class="relative card-button inline-flex p-0.5 mr-2 mb-0 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
-                            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        <button class="static card-button inline-flex p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+                            <span class="static px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                                 Edit
                             </span>
                         </button>
