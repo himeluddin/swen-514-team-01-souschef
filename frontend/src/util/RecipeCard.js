0cc0868e5f3b2afc2e0fd1b0d363e62846e410e0
Mackenzie Wade <mackenziewade@Mackenzies-MacBook-Air.local>
2024-03-31 20:32:24 -0400

    branch finished

--- frontend/src/util/RecipeCard.js
+++ frontend/src/util/RecipeCard.js
@@ -12,8 +12,8 @@ function RecipeCard({recipe,index2}) {
                 className="  grid grid-cols-2 shadow-md border-black border-2 place-items-center rounded-t w-1/2 h-full bg-white">
                 <div className={"font-InterExtraLight text-2xl font-bold"}>{recipe.title}</div>
                 <div className={"grid grid-row-2 place-items-center "}>
-                    <div>{index2}</div>
-                    <div>ingredients missing</div>
+                    <div>{recipe.ingredient_score}</div>
+                    <div>ingredients found</div>
                     <Link to= '/recipeInformation'> 
                         <button
                                     className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
